948630a0df304540e016fcaf416e9223a4e63d16
Bove <ribohe94@gmail.com>
2017-09-17 01:27:44 -0600

    First commit Add initial files API is configured in server/src/main/resources/api.yaml viva la liga :soccer:

---
+++ server/src/main/java/com/programathon/polling/PollingApplication.java
@@ -0,0 +1,12 @@
+package com.programathon.polling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PollingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PollingApplication.class, args);
+	}
+}
