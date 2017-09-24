f80032c5350f62e511fc94a32c82f3935a31ccd4
Xavier Fernandez <xavier@crservers.com>
2017-09-24 01:55:03 -0600

    gyeee

--- democra-tico/app/models/user.rb
+++ democra-tico/app/models/user.rb
@@ -1,4 +1,6 @@
+require 'securerandom'
 class User < ApplicationRecord
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable#, :confirmable, :lockable, :timeoutable
   devise :database_authenticatable, :registerable,
