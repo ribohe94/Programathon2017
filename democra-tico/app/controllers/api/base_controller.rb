887846114786806e3a31195e800794658aec6577
Xavier Fernandez <xavier@crservers.com>
2017-09-24 00:45:41 -0600

    change

--- democra-tico/app/controllers/api/base_controller.rb
+++ democra-tico/app/controllers/api/base_controller.rb
@@ -1,13 +1,15 @@
 class Api::BaseController < ApplicationController
-  # before_action :ensure_and_set_current_user!
+  before_action :ensure_and_set_current_user!
 
-  # def ensure_and_set_current_user!
+  def ensure_and_set_current_user!
+
+    puts params[:email]
   #   if current_user
   #     @current_user = current_user
   #   else
   #     render json: {}, status: :unauthorized
   #   end
-  # end
+  end
 
   private
 
