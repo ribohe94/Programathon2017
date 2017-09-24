6c04d0180f94ea6dce6647e409bf5cdc2e73e1a5
Jose Pablo Madrigal <pablomadrigaless@gmail.com>
2017-09-24 03:43:53 -0600

    event manager updated

--- P2017App/src/pages/usermanager/usermanager.ts
+++ P2017App/src/pages/usermanager/usermanager.ts
@@ -6,9 +6,21 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'usermanager.html'
 })
 export class UserManagerPage {
+  userForm = {
+    userName: '',
+    userLastName: '',
+    userSecondLastName: '',
+    email: '',
+    password: '',
+    confirmPassword: '',
+    birthDate: ''
 
+  }
   constructor(public navCtrl: NavController) {
 
+  }
+  createUser(){
+    console.log(this.userForm);
   }
 
 }
